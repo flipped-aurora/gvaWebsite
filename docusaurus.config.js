b47d5ca77665f62a983276598709f28eafd949a7
pixel <303176530@qq.com>
2020-08-24 15:32:58 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+module.exports = {
+    title: 'Gin-Vue-Admin',
+    tagline: 'gin+vue极速开发的全栈后台管理系统',
+    url: 'http://demo.henrongyi.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    favicon: 'img/favicon.ico',
+    organizationName: 'flipped-aurora', // Usually your GitHub org/user name.
+    projectName: 'gin-vue-admin', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Gin-Vue-Admin',
+            logo: {
+                alt: 'Gin-Vue-Admin',
+                src: 'img/logo.png',
+            },
+            items: [{
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Docs',
+                    position: 'left',
+                },
+                { to: 'blog', label: 'Blog', position: 'left' },
+                {
+                    href: 'https://github.com/flipped-aurora/gin-vue-admin',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [{
+                    title: '团队项目',
+                    items: [{
+                            label: 'Style Guide',
+                            to: 'docs/',
+                        },
+                        {
+                            label: 'Second Doc',
+                            to: 'docs/doc2/',
+                        },
+                    ],
+                },
+                {
+                    title: '社区',
+                    items: [{
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: '更多',
+                    items: [{
+                            label: 'Blog',
+                            to: 'blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} flipped-aurora | Powered by gin-vue-admin Copyright`,
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    // It is recommended to set document id as docs home page (`docs/` path).
+                    homePageId: 'doc1',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+};
