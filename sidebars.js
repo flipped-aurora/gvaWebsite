0600edb2b299faee90eea43a50fb82fb950d2b45
SliverHorn <503551462@qq.com>
2020-10-26 17:11:53 +0800

    update doc

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     firstSidebar: {
         '简介': ['introduce'],
         '基础使用': ['base', 'env', 'web', 'server', 'config', 'public'],
-        '详细教程': ['help', 'config-v2.3.0', 'help20200915'],
+        '代码生成器': ['auto_code'],
         '额外功能': ['extra', 'zap', 'oss', 'email', 'gorm', 'docker', 'docker-compose', 'new'],
         '捐赠': ['coffee']
     },
