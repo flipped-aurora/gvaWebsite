caf5d476ac881a8bddcde617087703388b6029fb
SliverHorn <sliver_horn@qq.com>
2021-03-06 22:19:13 +0800

    添加v2.4.0 的版本初始化方法

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
     firstSidebar: {
         '简介': ['introduce'],
-        '基础使用': ['base', 'env', 'web', 'server', 'config', 'public'],
+        '基础使用': ['base', 'env', 'server', 'web', 'config', 'public'],
         '代码生成器': ['auto_code'],
         '表单生成器': ['form_code'],
         '额外功能': ['extra', 'zap', 'oss', 'email', 'gorm', 'docker', 'docker-compose', 'new'],
