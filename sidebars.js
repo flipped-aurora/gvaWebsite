1a0c4a3e99999cc8d0e691ce06f3d8534c506401
蒋吉兆 <303176530@qq.com>
2021-04-10 23:34:36 +0800

    增加广告位招租和付费功能

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,10 @@
 module.exports = {
     firstSidebar: {
+        '捐赠': ['coffee','payment'],
         '简介': ['introduce'],
         '基础使用': ['base', 'env', 'server', 'web', 'config', 'public'],
         '代码生成器': ['auto_code'],
         '表单生成器': ['form_code'],
         '额外功能': ['extra', 'zap', 'oss', 'email', 'gorm', 'docker', 'docker-compose', 'new'],
-        '捐赠': ['coffee']
     },
 };
