1703f36b1bb952730dd4619f411a36b11073fbc2
tscuite <tscuite@qq.com>
2021-12-29 14:00:40 +0800

    添加from表单线上使用指南

--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,7 @@ module.exports = {
         '基础使用': ['base', 'env', 'server', 'server_video', 'web', 'web_video', 'config', 'develop', 'public','multiple_databases'],
         'K8s': ['deployment'],
         '代码生成器': ['auto_code'],
-        '表单生成器': ['form_code'],
+        '表单生成器': ['form_code','form_prod'],
         'GitHub': ['pulls'],
         '额外功能': ['extra', 'zap', 'oss', 'email', 'gorm', 'docker', 'docker-compose', 'new', 'timer'],
     },
